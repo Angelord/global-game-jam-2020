98e8d54e2187005dcb63a759602504fbf9fe116b
Angel Georgiev <angel.georgiev@diadraw.com>
2020-02-01 16:26:05 +0200

    Separate ranged attack behaviour.

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Construct))]
+public abstract class Attacker : MonoBehaviour {
+	
+	public float AttackRate;
+	public float Damage;
+
+	private Construct _construct;
+	private Senses _senses;
+	private ScrapBehaviour _currentTarget;
+	private float _lastAttack;
+	
+	private void Start() {
+		_construct = GetComponent<Construct>();
+		_senses = GetComponentInChildren<Senses>();
+		_senses.OnObjectEnter += OnObjectEnter;
+		_senses.OnObjectExit += OnObjectExit;
+	}
+
+	private void OnObjectEnter(ScrapBehaviour obj) {
+		if (_currentTarget == null) {
+			_currentTarget = _senses.GetAttackTarget(_construct.Faction);
+		}
+	}
+
+	private void OnObjectExit(ScrapBehaviour obj) {
+		if (_currentTarget == obj) {
+			_currentTarget = _senses.GetAttackTarget(_construct.Faction);
+		}
+	}
+
+	private void Update() {
+		if (_currentTarget == null) { return; }
+		
+		if (Time.time - _lastAttack >= AttackRate) {
+			Attack(_currentTarget);
+			_lastAttack = Time.time;
+		}
+	}
+
+	protected abstract void Attack(ScrapBehaviour target);
+}
--- Assets/Scripts/RangedAttacker.cs
+++ Assets/Scripts/RangedAttacker.cs
@@ -1,52 +1,16 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(Construct))]
-public class RangedAttacker : MonoBehaviour {
-
-	public float AttackRate;
-	public float Damage;
+public class RangedAttacker : Attacker {
+	
 	public GameObject Projectile;
 
-	private Construct _construct;
-	private Senses _senses;
-	private ScrapBehaviour _currentTarget;
-	private float _lastAttack;
-	
-	private void Start() {
-		_construct = GetComponent<Construct>();
-		_senses = GetComponentInChildren<Senses>();
-		_senses.OnObjectEnter += OnObjectEnter;
-		_senses.OnObjectExit += OnObjectExit;
-	}
-
-	private void OnObjectEnter(ScrapBehaviour obj) {
-		if (_currentTarget == null) {
-			_currentTarget = _senses.GetAttackTarget(_construct.Faction);
-		}
-	}
-
-	private void OnObjectExit(ScrapBehaviour obj) {
-		if (_currentTarget == obj) {
-			_currentTarget = _senses.GetAttackTarget(_construct.Faction);
-		}
-	}
-
-	private void Update() {
-		if (_currentTarget == null) { return; }
-		
-		if (Time.time - _lastAttack >= AttackRate) {
-			FireProjectile();
-			_lastAttack = Time.time;
-		}
-	}
-
-	private void FireProjectile() {
+	protected override void Attack(ScrapBehaviour target) {
 		Projectile proj = GameObject.Instantiate(Projectile, transform.position, Quaternion.identity).GetComponent<Projectile>();
 		
-		proj.Initialize(_currentTarget.transform, () => {
-			if (_currentTarget == null || _currentTarget.Attackable) { return; }
-			_currentTarget.TakeDamage(Damage);
+		proj.Initialize(target.transform, () => {
+			if (target == null || target.Attackable) { return; }
+			target.TakeDamage(Damage);
 		});
 	}
 }
