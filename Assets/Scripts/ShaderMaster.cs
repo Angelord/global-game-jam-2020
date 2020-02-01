4583fd2e5bf3a14206e7259dbf3227ebd1d0d39c
si3792 <simo.t.iliev@gmail.com>
2020-02-01 23:44:45 +0200

    Remove pause from ShadeMaster

--- Assets/Scripts/ShaderMaster.cs
+++ Assets/Scripts/ShaderMaster.cs
@@ -7,27 +7,23 @@ public class ShaderMaster : MonoBehaviour
     public List<Material> materials;
     Material originalMat;
 
+    public bool activateOnStart = false;
     public int _matIndex;
-    public float _pause;
     public float _duration;
    
 
     void Start()
     {
         originalMat = GetComponent<SpriteRenderer>().material;
-        SetShader(0, _pause, _duration);
-   
+        SetShader(_matIndex, _duration);
         
     }
 
 
-
-    public void SetShader(int index, float duration = 0, float pause = 0)
+    public void SetShader(int index, float duration = 0)
     {
-        // no pause means the effect ends after duration
         _matIndex = index;
         _duration = duration;
-        _pause = pause;
         ActivateMaterial();
     }
 
@@ -44,9 +40,5 @@ public class ShaderMaster : MonoBehaviour
     public void ResetMaterial()
     {
         GetComponent<SpriteRenderer>().material = originalMat;
-        if(_pause != 0)
-        {
-            Invoke("ActivateMaterial", _pause);
-        }
     }
 }
