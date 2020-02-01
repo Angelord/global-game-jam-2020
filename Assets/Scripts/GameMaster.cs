6d07b9c92d2e06c3bb312c20bbf8e894648231d0
si3792 <simo.t.iliev@gmail.com>
2020-02-02 00:16:41 +0200

    game master

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour
+{
+    public enum GameState
+    {
+        RUNNING,
+        PAUSED,
+        OVER
+    }
+
+    public static GameState state = GameState.RUNNING;
+
+    public static void ChangeState(GameState newState)
+    {
+        GameMaster.state = newState;
+        if (GameMaster.state == GameState.PAUSED || GameMaster.state == GameState.OVER)
+        {
+            Time.timeScale = 0f;
+        } else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+}
