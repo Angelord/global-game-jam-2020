3252ed7295640ad147e0a18fdaafb96109a5fa33
si3792 <simo.t.iliev@gmail.com>
2020-02-02 08:15:38 +0200

    Add unit spawner

---
+++ Assets/Scripts/UnitSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSpawner : MonoBehaviour
+{
+    public List<GameObject> spawnList;
+    public float spawnChance = 0.8f;
+    
+    void Start()
+    {
+        if(Random.Range(0, 1) <= spawnChance)
+        {
+            Instantiate(spawnList[Mathf.RoundToInt(Random.Range(1, 1000000)) % spawnList.Count], transform);
+        }
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
