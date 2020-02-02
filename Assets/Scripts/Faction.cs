dfe110783ca218e17ac47428ede725edb0db6928
si3792 <simo.t.iliev@gmail.com>
2020-02-02 15:32:58 +0200

    Desparate attempts at gaiki

--- Assets/Scripts/Faction.cs
+++ Assets/Scripts/Faction.cs
@@ -8,8 +8,9 @@ public class Faction : ScriptableObject {
 	public int wins = 0;
 	
 	private static Faction _neutral;
+    public GameObject repairFx;
 
-	public static Faction Neutral {
+    public static Faction Neutral {
 		get {
 			if (_neutral == null) {
 				_neutral = ScriptableObject.CreateInstance<Faction>();
