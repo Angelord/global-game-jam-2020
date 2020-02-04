c7a91cc9671ed3c67068b6253acd67c55830078e
Angelord <Georgiev1997@gmail.com>
2020-02-04 21:59:49 +0200

    Fix repair effect pivots and animation speeds. Add fade out.

---
+++ Assets/Scripts/FadeOutSprite.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class FadeOutSprite : MonoBehaviour {
+
+    public float Duration = 0.5f;
+    private float _timeRemaining;
+    private SpriteRenderer _renderer;
+    private float _startAlpha;
+    
+    private void Start() {
+        _timeRemaining = Duration;
+        _renderer = GetComponent<SpriteRenderer>();
+        _startAlpha = _renderer.color.a;
+    }
+
+    private void Update() {
+
+        _timeRemaining -= Time.deltaTime;
+
+        Color col = _renderer.color;
+        
+        if (_timeRemaining <= 0.0f) {
+            col.a = 0.0f;
+            this.enabled = false;
+        }
+        else {
+            col.a = _timeRemaining / Duration * _startAlpha;
+        }
+
+        _renderer.color = col;
+    }
+}
---
+++ Assets/Scripts/FadeOutSpriteWithAnimation.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class FadeOutSpriteWithAnimation : MonoBehaviour {
+
+    private SpriteRenderer _renderer;
+    private Animator _animator;
+    private float _initialAlpha;
+    
+    private void Start() {
+
+        _renderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+        _initialAlpha = _renderer.color.a;
+        
+        Invoke("Disable", this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
+    }
+
+    private void Update() {
+
+        Color col = _renderer.color;
+
+        col.a = _animator.GetCurrentAnimatorStateInfo(0).normalizedTime * _initialAlpha;
+    }
+    
+    private void Disable() {
+        Color col = _renderer.color;
+        col.a = 0.0f;
+        _renderer.color = col;
+
+        this.enabled = false;
+    }
+}
