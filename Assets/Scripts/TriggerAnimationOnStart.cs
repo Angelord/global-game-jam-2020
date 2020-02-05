3a9d89b9baf9590bd907b669f498fd011f107bc3
Angelord <Georgiev1997@gmail.com>
2020-02-06 00:15:42 +0200

    Fix showroom layout.

---
+++ Assets/Scripts/TriggerAnimationOnStart.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class TriggerAnimationOnStart : MonoBehaviour {
+
+    public string TriggerName;
+
+    private void Start() {
+        GetComponent<Animator>().SetTrigger(TriggerName);
+    }
+}
---
+++ Assets/Scripts/UserInterface/ShowroomGUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShowroomGUI : MonoBehaviour {
+    
+    private void Start() {
+        Time.timeScale = 1;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Return)) {
+            BackToMain();
+        }
+    }
+
+    public void BackToMain() {
+        SceneManager.LoadScene("MasterScene");
+    }
+}
