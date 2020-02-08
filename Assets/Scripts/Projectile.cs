21854d51780fe7960eb9e5439cd6fe44b0fdceba
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2020-02-08 10:16:42 +0200

    Add trail to Pistario projectiles.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class Projectile : MonoBehaviour, IProjectile {
 
@@ -20,7 +21,19 @@ public class Projectile : MonoBehaviour, IProjectile {
 		else if (UsesTeamColor) {
 			GetComponentInChildren<SpriteRenderer>().color = attacker.Faction.Color;
 		}
-		
+
+		// Team color the trail renderer if there is one
+		TrailRenderer trailRenderer = GetComponent<TrailRenderer>();
+		if (trailRenderer != null) {
+			
+			Gradient colorGradient = trailRenderer.colorGradient;
+			colorGradient.SetKeys(
+				new[] { new GradientColorKey(attacker.Faction.Color, 0.0f), new GradientColorKey(attacker.Faction.Color, 1.0f) },
+				colorGradient.alphaKeys
+			);
+			trailRenderer.colorGradient = colorGradient;
+		}
+
 		OnInitialize();
 	}
 
